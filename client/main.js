4f92e795b96902df1c1e26de43b7505c1f2768e7
loketa <yyuuqqii@outlook.com>
2017-09-20 17:08:11 +0800

    add authorized

--- client/main.js
+++ client/main.js
@@ -1,7 +1,7 @@
 import { Template } from 'meteor/templating';
 import { ReactiveDict } from 'meteor/reactive-dict';
+import { Tasks } from '../imports/api/tasks';
 
-import Tasks from '../imports/api/tasks.js';
 import '../imports/startup/accounts-config.js';
 
 import './main.html';
@@ -9,57 +9,58 @@ import './main.html';
 let bodyInstance = null;
 
 Template.body.events({
-  'submit .new-task'(event) {
-    // Prevent default browser form submit
-    event.preventDefault();
- 
-    // Get value from form element
-    const target = event.target;
-    const text = target.text.value;
- 
-    // Insert a task into the collection
-    Tasks.insert({
-      text,
-      createdAt: new Date(), // current time
-    });
- 
+    'submit .new-task'(event) {
+        // Prevent default browser form submit
+        event.preventDefault();
+
+        // Get value from form element
+        const target = event.target;
+        const text = target.text.value;
+
+        // Insert a task into the collection
+        Meteor.call('tasks.insert', text);
+
     // Clear form
-    target.text.value = '';
-  },
-  'change .hide-completed input'(event, instance) {
-    instance.state.set('hideCompleted', event.target.checked);
-  },
+        target.text.value = '';
+    },
+    'change .hide-completed input'(event, instance) {
+        instance.state.set('hideCompleted', event.target.checked);
+    },
 });
 
 Template.body.onCreated( () => {
-  bodyInstance = Template.instance();
-  bodyInstance.state =  new ReactiveDict();
+    bodyInstance = Template.instance();
+    bodyInstance.state =  new ReactiveDict();
 });
 
 Template.body.helpers({
   incompleteCount() {
-    return Tasks.find({ checked: { $ne: true } }).count();
+    return Tasks.find({
+        checked: { $ne: true },
+        owner:Meteor.userId()
+    }).count();
   },
 });
 
 Template.tasks.helpers({
   taskList() {
     if(bodyInstance.state.get('hideCompleted')) {
-      return Tasks.find({ checked: { $ne: true } }, { sort: { createdAt: -1 } });
+      return Tasks.find({
+          checked: { $ne: true },
+          owner:Meteor.userId()
+      }, { sort: { createdAt: -1 } });
     }
-    return Tasks.find({}, { sort: { createdAt: -1 } });
+    return Tasks.find({owner:Meteor.userId()}, { sort: { createdAt: -1 } });
   },
 });
 
 Template.tasks.events({
-  'click .toggle-checked'() {
-    Tasks.update(this._id, {
-      $set: { checked: !this.checked},
-    });
-  },
-  'click .delete'() {
-    Tasks.remove(this._id);
-  },  
+    'click .toggle-checked'() {
+        Meteor.call('tasks.setChecked', this._id, !this.checked);
+    },
+    'click .delete'() {
+        Meteor.call('tasks.remove', this._id);
+    },
 
 });
 
